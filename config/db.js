a9969e698f92d0e3a1ff35664934887b4e9b12f4
Max Chamberlain <maxchamberlain@Maxs-MacBook-Pro.local>
2022-10-04 11:48:29 -0600

    db

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async() => {
     try{
-        const conn = await mongoose.connect('mongodb+srv://admin:Administrator123@vidash.ckylswt.mongodb.net/?retryWrites=true&w=majority', {
+        const conn = await mongoose.connect('mongodb+srv://admin:Administrator123@vidash.ckylswt.mongodb.net/devault?retryWrites=true&w=majority', {
             useUnifiedTopology: true,
             useNewUrlParser: true,
         })
